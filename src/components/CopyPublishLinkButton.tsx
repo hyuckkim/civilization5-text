250953fc0d96f876500928058434ad7ad2fd3d31
hyuckkim <monkeyhyuck@gmail.com>
2024-01-14 11:11:42 +0900

    useOrigin

--- src/components/CopyPublishLinkButton.tsx
+++ src/components/CopyPublishLinkButton.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useOrigin } from "@/hooks/useOrigin";
+
 export function CopyPublishLinkButton({ binary, className }: { binary: string, className?: string}) {
-  const url = `${document.location.origin}/publish/text?d=${binary}`;
+  const origin = useOrigin();
+  const url = `${origin}/publish/text?d=${binary}`;
   return <button onClick={() => navigator.clipboard.writeText(url)} className={className}>텍스트 링크 복사</button>
 }
