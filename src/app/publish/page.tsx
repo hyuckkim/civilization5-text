21a43eee3674a20f03d801d405a2aedc25c66e46
hyuckkim <monkeyhyuck@gmail.com>
2024-01-14 14:59:31 +0900

    오타 수정

--- src/app/publish/page.tsx
+++ src/app/publish/page.tsx
@@ -11,13 +11,13 @@ const TextPage = ({
 }) => {
   const binary = searchParams?.d;
   const key = searchParams?.k;
-  
+
   const text = typeof binary === "string" 
     ? Base64.decode(binary)
     : typeof key === "string"
       ? getText(key, "Language_ko_KR").Text
       : "";
-0
+
   return (
     <Civ5RenderedText_Server str={text}/>
    );
