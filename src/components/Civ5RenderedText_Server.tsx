f92311ae941a823bed126a36a56d017db54bf192
hyuckkim <monkeyhyuck@gmail.com>
2024-01-24 23:59:56 +0900

    fix

--- src/components/Civ5RenderedText_Server.tsx
+++ src/components/Civ5RenderedText_Server.tsx
@@ -2,18 +2,19 @@ import react from 'react';
 import Civ5RenderedTextBlock from './Civ5RenderedTextBlock';
 import { getColor } from '@/db';
 import { Civ5RenderedTextProp, CivColors, CivSQLColor } from '@/types';
-import { prerenderer } from '@/utils';
+import { prerenderer, sliceBracket } from '@/utils';
 import * as regexp_misc from 'regexp-misc';
 
 export default async function Civ5RenderedText_Server({ str }: Civ5RenderedTextProp) {
   
   const sliced = regexp_misc.separate(str, /\[(.+?)\]/g);
   const {text, key} = prerenderer(sliced);
+  const { text: sepText, brackets } = sliceBracket(text);
   const colors = patchColorData(key)
     
   return (
       <div className='bg-black rounded-md p-2 border border-l-white max-w-xl'>{
-          text.map((e, idx) => <Civ5RenderedTextBlock text={e} colors={colors} key={idx} />)
+          sepText.map((e, idx) => <Civ5RenderedTextBlock text={e} colors={colors} brackets={brackets} key={idx} />)
       }
       </div>
   );
