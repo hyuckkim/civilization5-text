9075cd4f730bdc85230912bca1cfcb989b332044
hyuckkim <monkeyhyuck@gmail.com>
2024-02-02 12:12:29 +0900

    feat: load key-text by click key

--- src/components/ChangableRenderedText.tsx
+++ src/components/ChangableRenderedText.tsx
@@ -8,6 +8,15 @@ import { Base64 } from "js-base64";
 export default function ChangableRenderedText({text}: {text: string}) {
   const [str, setStr] = useState(text);
   const [brackets, setBrackets] = useState<string[][]>([]);
+
+  const loadText = (idx: number) => {
+    const key = brackets[idx][0].replace(/[{}]/g, "");
+    fetch(new URL(`/api/text/Language_ko_KR/${key}`, document.location.origin))
+      .then(async (value) => {
+        const text = await value.json();
+        setBrackets(prev => prev.map((v, i) => i === idx ? [v[0], text.Text] : v));
+      });
+  }
   
   return <>
     <textarea value={str} onChange={(e) => setStr(e.target.value)} className='w-full h-48 border-gray-200 border-2 box-border'/>
@@ -15,8 +24,8 @@ export default function ChangableRenderedText({text}: {text: string}) {
     <CopyPublishLinkButton binary={Base64.encode(str, true)} className="border-2 border-black rounded-md m-2 px-4 py-2"/>
     {brackets.map((b, i) => (
       <div key={i} className="w-2xl flex justify-between mb-0.5">
-        {b[0]}
-        <input value={b[1]} className="bg-neutral-200 w-96" onChange={(e) => setBrackets(prev =>
+        <div className="w-1/2 overflow-hidden text-ellipsis rtl text-sm" title={b[0]} onClick={() => loadText(i)}>{b[0]}</div>
+        <input value={b[1]} className="bg-neutral-200 w-1/2" onChange={(e) => setBrackets(prev =>
           prev.map((b, j) => (i === j) ? [b[0], e.target.value] : b))} />
       </div>
     ))}
